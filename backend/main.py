c84294d464774797e60cdf37d589ff30d9d36df8
Jakub Giżewski <76437687+giziuuu@users.noreply.github.com>
2025-06-11 19:50:36 +0200

    export pdf (#8)

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-from backend.routers import upload, quiz, evaluate
+from backend.routers import upload, quiz, evaluate, quiz_pdf
 
 app = FastAPI()
 
 app.include_router(upload.router, prefix="/upload", tags=["Upload"])
 app.include_router(quiz.router, prefix="/quiz", tags=["Quiz"])
 app.include_router(evaluate.router, prefix="/evaluate", tags=["Evaluate"])
+app.include_router(quiz_pdf.router, prefix="/quiz", tags=["QuizPDF"])
